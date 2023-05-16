ea131e525fb06906e006e2e0bd9600d93fe4c84b
Mark <voronkov1mark@gmail.com>
2023-05-16 23:46:25 +0300

    initial commit

---
+++ src/main/java/com/pro/sky/homeworklistemployee/HomeWorkListEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.pro.sky.homeworklistemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkListEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkListEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pro/sky/homeworklistemployee/HomeWorkListEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.homeworklistemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkListEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
